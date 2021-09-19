781217b2bbd0c634a70a7e7344feaf5b097222b7
omar chatin <omerchatin19@gmail.com>
2021-09-19 18:52:28 +0300

    changeing desgin

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-$('.burgerBtn').click(()=>{
-    $('.navRight').toggleClass('showNavBar')
 
+
+
+const toggleNavBar = ()=>{
+    $('.navRight').toggleClass('showNavRight')
     $('.line1').toggleClass('open')
     $('.line2').toggleClass('open')
     $('.line3').toggleClass('open')
+}
+$('.burgerBtn').click(()=>{toggleNavBar()})
+
+
+
+
+
+
+
+// navigation bar scrolling -START-
+$('.about').click(()=>{
+    $(".sec2")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+    toggleNavBar()
 })
+
+
+$('.home').click(()=>{
+    $(".sec1")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "start" // or "end"
+    });
+    toggleNavBar()
+})
+// navigation bar scrolling -END-
