4d7468b2a381a3df364e359b2949162bafe3a8dd
omar chatin <omerchatin19@gmail.com>
2021-09-19 16:54:49 +0300

    adding navigation bar

---
+++ app.js
@@ -0,0 +1,7 @@
+$('.burgerBtn').click(()=>{
+    $('.navRight').toggleClass('showNavBar')
+
+    $('.line1').toggleClass('open')
+    $('.line2').toggleClass('open')
+    $('.line3').toggleClass('open')
+})
