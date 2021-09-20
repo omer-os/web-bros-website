1ddb71cf06b7f01b725aaf9d7ca9c690c8afac40
omar chatin <omerchatin19@gmail.com>
2021-09-21 00:41:28 +0300

    nearly complete

--- app.js
+++ app.js
@@ -15,7 +15,21 @@ $('.burgerBtn').click(()=>{toggleNavBar()})
 
 
 
+
+
 // navigation bar scrolling -START-
+
+
+$('.home').click(()=>{
+    $(".sec1")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "end" // or "end"
+    });
+    toggleNavBar()
+})
+
+
+
 $('.services').click(()=>{
     $(".sec2-title")[0].scrollIntoView({
         behavior: "smooth", // or "auto" or "instant"
@@ -25,11 +39,23 @@ $('.services').click(()=>{
 })
 
 
-$('.home').click(()=>{
-    $(".sec1")[0].scrollIntoView({
+
+
+$('.contact').click(()=>{
+    $(".sec4")[0].scrollIntoView({
         behavior: "smooth", // or "auto" or "instant"
-        block: "start" // or "end"
+        block: "end" // or "end"
     });
     toggleNavBar()
 })
+
+$('.projects').click(()=>{
+    $(".sec3")[0].scrollIntoView({
+        behavior: "smooth", // or "auto" or "instant"
+        block: "end" // or "end"
+    });
+    toggleNavBar()
+})
+
+
 // navigation bar scrolling -END-
